6bbcbdcdb6daf3cc0234c8cd10587fa2cca9779c
Mauro Sagasti <maurosagasti@gmail.com>
2024-11-22 04:42:44 -0300

    Primer commit

---
+++ src/main/java/com/maurosagasti/api_sistema_academico/ApiSistemaAcademicoApplication.java
@@ -0,0 +1,13 @@
+package com.maurosagasti.api_sistema_academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maurosagasti/api_sistema_academico/ApiSistemaAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurosagasti.api_sistema_academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
